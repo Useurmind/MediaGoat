71bf59d8403821af5143c26c99319a246b703826
Jochen Grün <jochen.gruen@googlemail.com>
2017-07-20 09:50:46 +0200

    Implemented simple page to show songs.

---
+++ MediaGoat/ClientApp/components/Songs.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import { Link, RouteComponentProps } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { ApplicationState } from '../store';
+import * as SongsStore from '../store/Songs';
+
+interface SongsUiState {
+    searchString: string
+}
+
+type SongsProps =
+    SongsStore.SongsState
+    & typeof SongsStore.actionCreators
+    & RouteComponentProps<{}>;
+
+class Songs extends React.Component<SongsProps, SongsUiState> {
+    constructor(props: SongsProps) {
+        super(props);
+
+        this.state = {
+            searchString: ""
+        };
+    }
+
+
+    public render() {
+        return <div>
+            <h1>Songs</h1>
+
+            <p>Search for songs.</p>
+
+            <input type="text" onChange={(e) => this.setState({ searchString: e.target.value })} />
+            <button onClick={() => { this.props.searchSongs(this.state.searchString) }}>Search</button>
+
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Artist</th>
+                        <th>Album</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        this.props.songs.map(s =>
+                            <tr key={s.title}>
+                                <td>{s.title}</td>
+                                <td>{s.artist}</td>
+                                <td>{s.album}</td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>
+        </div>;
+    }
+}
+
+// Wire up the React component to the Redux store
+export default connect(
+    (state: ApplicationState) => state.songs, // Selects which state properties are merged into the component's props
+    SongsStore.actionCreators                 // Selects which action creators are merged into the component's props
+)(Songs) as typeof Songs;
---
+++ MediaGoat/ClientApp/store/Songs.ts
@@ -0,0 +1,76 @@
+import { Action, Reducer } from 'redux';
+import { AppThunkAction } from './';
+import { fetch, addTask } from 'domain-task';
+
+// -----------------
+// STATE - This defines the type of data maintained in the Redux store.
+
+export interface Song {
+    title: string;
+    artist: string;
+    album: string;
+}
+
+export interface SongsState {
+    songs: Song[];
+    isLoading: boolean;
+    searchString: string;
+}
+
+// -----------------
+// ACTIONS - These are serializable (hence replayable) descriptions of state transitions.
+// They do not themselves have any side-effects; they just describe something that is going to happen.
+// Use @typeName and isActionType for type detection that works even after serialization/deserialization.
+
+interface SearchSongsAction { type: 'SearchSongsAction', searchString: string }
+interface ReceiveSongsAction { type: 'ReceiveSongsAction', songs: Song[] }
+
+// Declare a 'discriminated union' type. This guarantees that all references to 'type' properties contain one of the
+// declared type strings (and not any other arbitrary string).
+type KnownAction = SearchSongsAction | ReceiveSongsAction;
+
+// ----------------
+// ACTION CREATORS - These are functions exposed to UI components that will trigger a state transition.
+// They don't directly mutate state, but they can have external side-effects (such as loading data).
+
+export const actionCreators = {
+    searchSongs: (searchString: string): AppThunkAction<KnownAction> => (dispatch, getState) => {
+        // Only load data if it's something we don't already have (and are not already loading)
+        let fetchTask = fetch(`/api/Song/Songs?searchString=${searchString}`)
+            .then(response => response.json() as Promise<Song[]>)
+            .then(data => {
+                dispatch({ type: 'ReceiveSongsAction', songs: data });
+            });
+
+        addTask(fetchTask); // Ensure server-side prerendering waits for this to complete
+        dispatch({ type: 'SearchSongsAction', searchString: searchString });
+    },
+    receiveSongs: (songs: Song[]) => <ReceiveSongsAction>{ type: 'ReceiveSongsAction', songs: songs }
+};
+
+// ----------------
+// REDUCER - For a given state and action, returns the new state. To support time travel, this must not mutate the old state.
+
+export const reducer: Reducer<SongsState> = (state: SongsState, action: KnownAction) => {
+    switch (action.type) {
+        case 'SearchSongsAction':
+            return {
+                songs: state.songs,
+                isLoading: true,
+                searchString: action.searchString
+            };
+        case 'ReceiveSongsAction':
+            return {
+                songs: action.songs,
+                isLoading: false,
+                searchString: state.searchString
+            };
+        default:
+            // The following line guarantees that every action in the KnownAction union has been covered by a case above
+            const exhaustiveCheck: never = action;
+    }
+
+    // For unrecognized actions (or in cases where actions have no effect), must return the existing state
+    //  (or default initial state if none was supplied)
+    return state || { songs: [], isLoading: false, searchString: "" };
+};
